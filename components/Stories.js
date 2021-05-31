43b6a1296f88f8837de33ab7c7d1aefe6b7610d2
Raima Hossain <dragdo3@gmail.com>
2021-05-31 00:00:21 -0700

    second commit

---
+++ components/Stories.js
@@ -0,0 +1,41 @@
+import StoryCard from "./StoryCard";
+
+const stories = [
+    {
+        name: "Raima Hossain",
+        src: "/../public/assets/foto.jpg",
+        profile: "/../public/assets/fb_dp.jpg",
+    },
+    {
+        name: "Bill Gates",
+        src: "https://links.papareact.com/4u4",
+        profile: "https://links.papareact.com/zvy",
+    },
+    {
+        name: "Mark Zuckerberg",
+        src: "https://links.papareact.com/xql",
+        profile: "https://links.papareact.com/snf",
+    },
+    {
+        name: "Jeff Bezos",
+        src: "https://links.papareact.com/k2j",
+        profile: "https://links.papareact.com/f0p",
+    },
+    {
+        name: "Elon Musk",
+        src: "https://links.papareact.com/4zn",
+        profile: "https://links.papareact.com/kxk",
+    },
+];
+
+function Stories() {
+    return (
+        <div className="flex justify-center space-x-3 mx-auto">
+            {stories.map((story) => (
+                <StoryCard key={story.src} name={story.name} src={story.src} profile={story.profile}/>
+            ))}
+        </div>
+    )
+}
+
+export default Stories;
---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    images: {
+        domains: [
+            "platform-lookaside.fbsbx.com",
+            "firebasestorage.googleapis.com",
+            "links.papareact.com",
+        ],
+    },
+};
