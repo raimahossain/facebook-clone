c42d69d99a00513109d5f39484b44348cb39eaac
Raima Hossain <dragdo3@gmail.com>
2021-06-02 18:59:27 -0700

    adding firebase.js file

--- firebase.js
+++ firebase.js
@@ -2,7 +2,12 @@ import firebase from "firebase";
 import "firebase/storage";
 
 const firebaseConfig = {
-    
+    apiKey: "AIzaSyD5juA6u6PHx1xRcAfQTdqZA7P6TaGX9Bo",
+    authDomain: "facebook-clone-82bf4.firebaseapp.com",
+    projectId: "facebook-clone-82bf4",
+    storageBucket: "facebook-clone-82bf4.appspot.com",
+    messagingSenderId: "836526568524",
+    appId: "1:836526568524:web:487a6fdb2f7adc76c730e9"
 };
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
